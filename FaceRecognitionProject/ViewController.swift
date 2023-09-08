f54ed55798342418adf58fbafb8e184958dc4be7
Atakan Aktakka <88386838+Atakan-Aktakka@users.noreply.github.com>
2023-09-08 16:32:33 +0300

    Add files via upload

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Atakan Aktakka on 8.09.2023.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                if success == true {
+                    //başarılı giriş
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                    
+                    
+                }else{
+                    DispatchQueue.main.async{
+                        self.myLabel.text = "Error"
+                    }
+                    
+                }
+            }
+            
+        }
+        
+        
+    }
+    
+}
+
